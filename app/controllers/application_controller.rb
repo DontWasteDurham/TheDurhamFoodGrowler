d0d5acfc2bd69f88f6450213c736276fa178cbed
damianhouse <damianhouse@gmail.com>
2016-09-06 12:04:50 -0400

    White list devise parameters for user schema.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :email, :password, :password_confirmation, :business_account, :purchased_boxes, :box_status, :exp_date])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :email, :password, :password_confirmation, :business_account, :purchased_boxes, :box_status, :exp_date])
+  end
 end
