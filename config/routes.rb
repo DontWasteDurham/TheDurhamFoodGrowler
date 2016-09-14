903e943418404f6befc5cec79a414803274955af
damianhouse <damianhouse@gmail.com>
2016-09-14 09:25:53 -0400

    Add user show page, routing, and style page.

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   get 'landing' =>'pages#landing'
   #Trying to get to a sign_out#
   #get 'users/sign_out' =>'devise/sessions#destroy'#
+  get 'user' => 'users#show'
 
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
