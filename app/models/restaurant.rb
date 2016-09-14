40834569ebb9616a4475bb58261888f3f0cf1a7e
damianhouse <damianhouse@gmail.com>
2016-09-13 21:33:09 -0400

    Adjust styling for mobile across application.

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,4 +2,5 @@ class Restaurant < ApplicationRecord
 
   has_many :transactions
   belongs_to :user
+  
 end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,10 +1,11 @@
 class Transaction < ApplicationRecord
-
+  
   belongs_to :user
   belongs_to :restaurant
+  validates :user_id, presence: true
+  validates :restaurant_id, presence: true
   validates :number_of_boxes, presence: true
   validates :restaurant_id, presence: true
   validates :user_id, presence: true
-
 
 end
