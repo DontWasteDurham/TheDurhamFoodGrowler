ed9990957e41d7b76871c0ad12c9b00565d554cc
damianhouse <damianhouse@gmail.com>
2016-11-13 16:44:30 -0500

    Add users controller and views.

--- config/routes.rb
+++ config/routes.rb
@@ -18,5 +18,6 @@ Rails.application.routes.draw do
   get 'user' => 'users#show'
 
   devise_for :users, controllers: {registrations: "registrations"}
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
